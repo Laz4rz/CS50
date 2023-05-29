a41fb73102063846cafab978e3cb1a4bfdfb2ad8
mikolaj <boronskimikolaj@gmail.com>
2023-05-29 15:53:36 +0200

    add abstractions and types part

---
+++ src/04_abstractions.c
@@ -0,0 +1,56 @@
+// CS50 Lecture 1 - abstractions and types
+
+/* 
+	bool, a Boolean expression of either true or false
+	char, a single character like a or 2
+	double, a floating-point value with more digits than a float
+	float, a floating-point value, or real number with a decimal value
+	int, integers up to a certain size, or number of bits
+	long, integers with more bits, so they can count higher than an int
+	string, a string of characters
+*/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int const GLOBAL_SIZE_LIMIT = 10;
+
+int get_size(char message[]);
+void print_grid(int width, int height);
+
+int main(void)
+{
+	printf("Global size limit of your blocks is %i", GLOBAL_SIZE_LIMIT);
+
+	int width = get_size("Gimme width");
+	int height = get_size("Gimme height");
+
+	print_grid(width, height);
+}
+
+int get_size(char message[])
+{
+	printf("%s", message);
+	printf("\n");
+
+	int n;
+	do
+	{
+		n = get_int("Size: ");
+	}
+	while(n < 1 && n > GLOBAL_SIZE_LIMIT);
+
+	return n;
+}
+
+void print_grid(int width, int height)
+{
+	for(int i=0; i<height; i++)
+	{
+		for(int j=0; j<width; j++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
