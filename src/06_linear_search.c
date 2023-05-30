a353a28f81197c0bf0a82f1d5907c02a71e3d5d9
mikolaj <mikolaj@montec-d16>
2023-05-30 15:47:18 +0200

    add search, data structures parts from lecture 3

---
+++ src/06_linear_search.c
@@ -0,0 +1,38 @@
+// CS50 Lecture 3 - Linear search
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    // Define an array of integers
+    int numbers[] = {168, 1000, 1, 25, 337, 50};
+
+    // Search for user defined number (linear)
+    int n = get_int("Enter a number: ");
+    for (int i = 0; i < 6; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found %i at position %i\n", n, i);
+            // return 0;
+        }
+    }
+    printf("Couldn't find %i\n", n);
+    // return 1;
+
+    // Search for user defined string (linear)
+    string strings[] = {"abba", "baba", "tata", "armata"};
+
+    string s = get_string("Enter a string: ");
+    for (int i = 0; i < 4; i++)
+    {
+        if (strcmp(strings[i], s) == 0) // strings cannot be compared by using ==, instead strcmp has to be used 
+        {
+            printf("Found %s at position %i\n", s, i);
+            // return 0;
+        }
+    }
+    // return 1;
+}
---
+++ src/07_data_structures.c
@@ -0,0 +1,38 @@
+// CS50 Lecture 3 - Data Structures
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    // Create an array of two person data structues called people
+    person people[2];
+
+    // Populate people with data
+    people[0].name = "Pawel";
+    people[0].number = "600 100 700";
+
+    people[0].name = "Jan";
+    people[0].number = "200 100 307";
+
+    // Search for name
+    string name = get_string("Find: ");
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].name, name) == 0)
+        {
+            printf("Found %s with %s\n", people[i].name, people[i].number);
+            return 0;
+        }
+    }
+    return 1;
+}
